17582cb4f27d1b750c0e3c1bf3f3e09d360b59b7
Charles Anderson <charlesandreanderson22@gmail.com>
2017-10-18 15:51:29 -0700

    merged

--- routes/sports.js
+++ routes/sports.js
@@ -1,17 +1,20 @@
 const
-express = require('express'),
-sportsRouter = new express.Router(),
+express = require('express')
+sportsRouter = new express.Router()
 passport = require('passport')
 httpClient = require('request')
+require('dotenv').load()
+key = process.env.sportsKey
 
 sportsRouter.route('/')
 .get((req, res) => {
-  var apiUrl = `http://api.sportradar.us/nfl-ot2/games/2017/REG/7/schedule.json?api_key=ptr58dz7pn2z8mdxbqrcsqdj`
+  var apiUrl = `http://api.sportradar.us/nfl-ot2/games/2017/REG/7/schedule.json?api_key=${key}`
   httpClient.get(apiUrl, (err, response, body) => {
       var data = JSON.parse(body)
       res.json(data)
     //   console.log(data.week.games[2].home.alias) //test 
   })
 })
+
 
 module.exports = sportsRouter
