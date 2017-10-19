810e870a641d1b4a9f526954f64e31ffdb8f29c8
Charles Anderson <charlesandreanderson22@gmail.com>
2017-10-19 15:08:52 -0700

    finished sportsTest

--- routes/sports.js
+++ routes/sports.js
@@ -4,7 +4,8 @@ sportsRouter = new express.Router()
 passport = require('passport')
 httpClient = require('request')
 require('dotenv').load()
-key = process.env.sportsKey
+key = process.env.sportsKey,
+statsKey = process.env.statsKey
 
 sportsRouter.route('/')
 .get((req, res) => {
@@ -19,14 +20,12 @@ sportsRouter.route('/')
 
 sportsRouter.route('/stats/:id')
 .get((req, res) => {
-  // var statsUrl = `http://api.sportradar.us/nfl-ot2/seasontd/2017/REG/teams/33405046-04ee-4058-a950-d606f8c30852/statistics.json?api_key=${key}`
-  
-  var statsUrl = `https://api.sportradar.us/nfl-ot2/seasontd/2017/REG/teams/${req.params.id}/statistics.json?api_key=${key}`
-  httpClient.get(statsUrl, (err, response, body) => {
+  var statUrl = `http://api.sportradar.us/nfl-ot2/seasontd/2017/REG/teams/${req.params.id}/statistics.json?api_key=nrqn8ufuv3w8hbswrjq8bvq5`
+  httpClient.get(statUrl, (err, response, body) => {
     if(err) return console.log(err)
-      // var data = JSON.parse(body)
-      console.log(body)
-      res.json(body)
+      var data = JSON.parse(body)
+      // console.log(body)
+      res.json(data)
   })
 })
 
